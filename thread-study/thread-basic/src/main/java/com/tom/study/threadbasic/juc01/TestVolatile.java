f92007d521ec34be29dd1f75e446378e1e430523
tangchunbo <532066967@qq.com>
2019-08-17 14:30:33 +0800

    init

---
+++ thread-study/thread-basic/src/main/java/com/tom/study/threadbasic/juc01/TestVolatile.java
@@ -0,0 +1,42 @@
+package com.tom.study.threadbasic.juc01;
+
+public class TestVolatile {
+
+    public static void main(String[] args) {
+        ThreadDemo td = new ThreadDemo();
+        new Thread(td).start();
+        while (true) {
+            if (td.getFlag()) {
+                System.out.println("------------------");
+                break;
+            }
+        }
+    }
+
+
+
+}
+
+class ThreadDemo implements Runnable {
+
+//    private volatile boolean flag = false;
+    private boolean flag = false;
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        flag = true;
+        System.out.println("flag : " + getFlag());
+
+    }
+
+    public boolean getFlag() {
+        return flag;
+    }
+
+}
