9b7a9f55962643b4c4484d101e3db0d31cb8898a
tangchunbo <532066967@qq.com>
2019-08-17 14:29:27 +0800

    init

---
+++ thread-study/disruptor-study/src/main/java/com/tom/study/disruptorstudy/DisruptorStudyApplication.java
@@ -0,0 +1,13 @@
+package com.tom.study.disruptorstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisruptorStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisruptorStudyApplication.class, args);
+    }
+
+}
---
+++ thread-study/thread-basic/src/main/java/com/tom/study/threadbasic/ThreadBasicApplication.java
@@ -0,0 +1,13 @@
+package com.tom.study.threadbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadBasicApplication.class, args);
+    }
+
+}
