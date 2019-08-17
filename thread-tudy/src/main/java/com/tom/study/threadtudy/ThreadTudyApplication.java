9834c85f1b8465de266cb294657f115b8259abb9
tangchunbo <532066967@qq.com>
2019-08-17 12:47:03 +0800

    init

---
+++ thread-tudy/src/main/java/com/tom/study/threadtudy/ThreadTudyApplication.java
@@ -0,0 +1,13 @@
+package com.tom.study.threadtudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadTudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadTudyApplication.class, args);
+    }
+
+}
---
+++ thread-tudy/src/test/java/com/tom/study/threadtudy/ThreadTudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tom.study.threadtudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreadTudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
